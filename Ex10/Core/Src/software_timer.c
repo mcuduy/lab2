f2019a4c9838572ccbdecc46d406e1d50689f90b
Anh Duy <anhduyytt@gmail.com>
2024-10-01 16:21:38 +0700

    first commit

---
+++ Ex10/Core/Src/software_timer.c
@@ -0,0 +1,56 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: Oct 1, 2024
+ *      Author: ADMIN
+ */
+
+#include "software_timer.h"
+
+int TIMER_CYCLE = 10;
+
+int timer1_counter = 0;
+int timer1_flag = 0;
+
+int timer2_counter = 0;
+int timer2_flag = 0;
+
+int timer3_counter = 0;
+int timer3_flag = 0;
+
+void setTimer1(int duration) {
+	timer1_counter = duration/TIMER_CYCLE;
+	timer1_flag = 0;
+}
+
+void setTimer2(int duration) {
+	timer2_counter = duration/TIMER_CYCLE;
+	timer2_flag = 0;
+}
+
+void setTimer3(int duration) {
+	timer3_counter = duration/TIMER_CYCLE;
+	timer3_flag = 0;
+}
+
+
+void timerRun() {
+	if(timer1_counter > 0) {
+		timer1_counter--;
+		if(timer1_counter <= 0) {
+			timer1_flag = 1;
+		}
+	}
+	if(timer2_counter > 0) {
+		timer2_counter--;
+		if(timer2_counter <= 0) {
+			timer2_flag = 1;
+		}
+	}
+	if(timer3_counter > 0) {
+		timer3_counter--;
+		if(timer3_counter <= 0) {
+			timer3_flag = 1;
+		}
+	}
+}
